88ffaa436fcdc5bbbc737bf39f43372c3952640b
Vinicius Alexandre <viniciusalexfernandes@gmail.com>
2023-09-26 09:49:34 -0300

    Atualizando classes

--- composicao/src/entities/Trabalhador.java
+++ composicao/src/entities/Trabalhador.java
@@ -1,5 +1,65 @@
 package entities;
 
-public class Trabalhador {
+import java.util.ArrayList;
+import java.util.List;
 
+import entities.enums.NivelTrabalhador;
+
+public class Trabalhador {
+	private String nome;
+	private NivelTrabalhador nivel;
+	private Double baseSalario;
+	
+	private Departamento departamento;
+	private List<HoraContrato> contratos = new ArrayList<>();
+	
+	public Trabalhador(){
+		
+	}
+
+	public Trabalhador(String nome, NivelTrabalhador nivel, Double baseSalario, Departamento departamento) {
+		this.nome = nome;
+		this.nivel = nivel;
+		this.baseSalario = baseSalario;
+		this.departamento = departamento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public NivelTrabalhador getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(NivelTrabalhador nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getBaseSalario() {
+		return baseSalario;
+	}
+
+	public void setBaseSalario(Double baseSalario) {
+		this.baseSalario = baseSalario;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<HoraContrato> getContratos() {
+		return contratos;
+	}
+
+	
+	
 }
